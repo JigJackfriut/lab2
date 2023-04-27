c37f42266c5a2b71e28b5af5a668fd7fb2d102e5
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2023-04-26 21:41:03 -0400

    Update restChat.js

--- restChat.js
+++ restChat.js
@@ -1,111 +1,106 @@
 $(document).ready(function() {
-    // Register Form Submit
-    $('#register-form').submit(function(event) {
-        // Prevent default form submission
-        event.preventDefault();
+    // Function to get chat messages and current users
+    function getMessagesAndUsers() {
+        // Make a GET request to /chat/messages endpoint
+        $.ajax({
+            url: '/chat/messages',
+            method: 'GET',
+            dataType: 'json',
+            success: function(response) {
+                // Display messages in #chat-messages div
+                var messages = response.messages;
+                var messageHtml = '';
+                for (var i = 0; i < messages.length; i++) {
+                    messageHtml += '<p><strong>' + messages[i].username + ':</strong> ' + messages[i].message + '</p>';
+                }
+                $('#chat-messages').html(messageHtml);
 
-        // Get form data
+                // Display current users in #current-users div
+                var users = response.users;
+                var usersHtml = '<p>Current users:</p><ul>';
+                for (var j = 0; j < users.length; j++) {
+                    usersHtml += '<li>' + users[j] + '</li>';
+                }
+                usersHtml += '</ul>';
+                $('#current-users').html(usersHtml);
+            },
+            error: function(xhr, status, error) {
+                console.log(error);
+            }
+        });
+    }
+
+    // Call getMessagesAndUsers function on page load
+    getMessagesAndUsers();
+
+    // Set up interval to call getMessagesAndUsers function every 5 seconds
+    setInterval(getMessagesAndUsers, 5000);
+
+    // Function to send a chat message
+    function sendMessage() {
+        // Get the chat message from the input field
+        var message = $('#chat-message').val();
+
+        // Make a POST request to /chat/messages endpoint
+        $.ajax({
+            url: '/chat/messages',
+            method: 'POST',
+            data: {
+                message: message
+            },
+            dataType: 'json',
+            success: function(response) {
+                // Do something with the response (if needed)
+            },
+            error: function(xhr, status, error) {
+                console.log(error);
+            }
+        });
+
+        // Clear the input field
+        $('#chat-message').val('');
+    }
+
+    // Call sendMessage function when chat form is submitted
+    $('#chat-form').submit(function(event) {
+        event.preventDefault();
+        sendMessage();
+    });
+
+    // Function to register a new user
+    function registerUser() {
+        // Get the registration form data
         var username = $('#register-username').val();
         var email = $('#register-email').val();
         var password = $('#register-password').val();
 
-        // Send AJAX request to register user
+        // Make a POST request to /chat/register endpoint
         $.ajax({
-            url: '/chat/register/' + username + '/' + email + '/' + password,
-            type: 'POST',
-            success: function(response) {
-                if (response.status == 'success') {
-                    alert('Registration successful!');
-                } else {
-                    alert('Registration failed: ' + response.error);
-                }
+            url: '/chat/register',
+            method: 'POST',
+            data: {
+                username: username,
+                email: email,
+                password: password
             },
-            error: function() {
-                alert('Registration failed. Please try again later.');
+            dataType: 'json',
+            success: function(response) {
+                // Do something with the response (if needed)
+            },
+            error: function(xhr, status, error) {
+                console.log(error);
             }
         });
-    });
 
-    // Login Form Submit
-    $('#login-form').submit(function(event) {
-        // Prevent default form submission
+        // Clear the registration form fields
+        $('#register-username').val('');
+        $('#register-email').val('');
+        $('#register-password').val('');
+    }
+
+    // Call registerUser function when registration form is submitted
+    $('#register-form').submit(function(event) {
         event.preventDefault();
-
-        // Get form data
-        var username = $('#login-username').val();
-        var password = $('#login-password').val();
-
-        // Send AJAX request to login user
-        $.ajax({
-            url: '/chat/login/' + username + '/' + password,
-            type: 'POST',
-            success: function(response) {
-                if (response.status == 'success') {
-                    alert('Login successful!');
-                } else {
-                    alert('Login failed: ' + response.error);
-                }
-            },
-            error: function()
-            alert('Login failed. Please try again later.');
-        }
+        registerUser();
     });
-});
-
-// Chat Form Submit
-$('#chat-form').submit(function(event) {
-    // Prevent default form submission
-    event.preventDefault();
-
-    // Get form data
-    var message = $('#chat-message').val();
-
-    // Send AJAX request to send chat message
-    $.ajax({
-        url: '/chat/send/' + message,
-        type: 'POST',
-        success: function(response) {
-            if (response.status == 'success') {
-                // Clear chat message input field
-                $('#chat-message').val('');
-            } else {
-                alert('Chat message failed to send.');
-            }
-        },
-        error: function() {
-            alert('Chat message failed to send. Please try again later.');
-        }
-    });
-});
-
-// Update Chat Messages and Current Users
-setInterval(function() {
-    // Send AJAX request to get chat messages and current users
-    $.ajax({
-        url: '/chat/update',
-        type: 'GET',
-        success: function(response) {
-            if (response.status == 'success') {
-                // Update chat messages
-                var messagesHtml = '';
-                for (var i = 0; i < response.messages.length; i++) {
-                    var message = response.messages[i];
-                    messagesHtml += '<p><strong>' + message.username + ':</strong> ' + message.message + '</p>';
-                }
-                $('#chat-messages').html(messagesHtml);
-
-                // Update current users
-                var usersHtml = '';
-                for (var i = 0; i < response.users.length; i++) {
-                    var user = response.users[i];
-                    usersHtml += '<p>' + user.username + '</p>';
-                }
-                $('#current-users').html(usersHtml);
-            }
-        },
-        error: function() {
-            alert('Failed to update chat messages and current users.');
-        }
-    });
-}, 1000);
 });
