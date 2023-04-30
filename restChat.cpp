a92074c8936eb232b38c3901a1909c077a838739
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2023-04-30 14:42:15 -0400

    Update restChat.cpp

--- restChat.cpp
+++ restChat.cpp
@@ -1,92 +1,234 @@
+////  namesAPI.cc - a microservice demo program
+//
+// James Skon
+// Kenyon College, 2022
+//
+
 #include <iostream>
-#include <string>
+#include <fstream>
+#include <map>
 #include <vector>
+#include <algorithm>
 #include "httplib.h"
+#include "ChatDB.h"
+#include "ChatEntry.h"
+//#include "TokenGenerator.h"
 
-using namespace std;
 using namespace httplib;
+using namespace std;
 
-struct User {
-    string username;
-    string password;
-    bool loggedIn;
-};
+const int port = 5005;
 
-vector<User> users;
 
-// Function to check if a username exists in the user list
-bool usernameExists(string username) {
-    for (User user : users) {
-        if (user.username == username) {
-            return true;
-        }
+
+std::string generateToken()
+{
+    std::string token;
+    const std::string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, chars.size() - 1);
+    for (int i = 0; i < 16; ++i) {
+        token += chars[dis(gen)];
     }
-    return false;
+    return token;
 }
 
-// Function to check if a username and password match a user in the user list
-bool authenticateUser(string username, string password) {
-    for (User& user : users) { // Use reference to modify login status
-        if (user.username == username && user.password == password) {
-            user.loggedIn = true;
-            return true;
-        }
-    }
-    return false;
+
+void addMessage(string username, string message, map<string,vector<string>> &messageMap) {
+	/* iterate through users adding message to each */
+	string jsonMessage = "{\"user\":\""+username+"\",\"message\":\""+message+"\"}";
+	for (auto userMessagePair : messageMap) {
+		username = userMessagePair.first;
+		messageMap[username].push_back(jsonMessage);
+	}
 }
 
-void handleRegistration(const Request& req, Response& res) {
-    string username = req.get_param_value("username");
-    string password = req.get_param_value("password");
-    if (usernameExists(username)) {
-        res.set_content("Username already exists.", "text/plain");
-    } else {
-        User newUser = {username, password, false};
-        users.push_back(newUser);
-        res.set_content("Registration successful!", "text/plain");
-    }
+string getMessagesJSON(string username, map<string,vector<string>> &messageMap) {
+	/* retrieve json list of messages for this user */
+	bool first = true;
+	string result = "{\"messages\":[";
+	for (string message :  messageMap[username]) {
+		if (not first) result += ",";
+		result += message;
+		first = false;
+	}
+	result += "]";
+	messageMap[username].clear();
+	return result;
 }
 
-void handleLogin(const Request& req, Response& res) {
-    string username = req.get_param_value("username");
-    string password = req.get_param_value("password");
-    if (authenticateUser(username, password)) {
-        res.set_content("Login successful!", "text/plain");
-    } else {
-        res.set_content("Invalid username or password.", "text/plain");
-    }
+string getusersJSON(string username, map<string,vector<string>> &messageMap) {
+	/* retrieve json list of messages for this user */
+	bool first = true;
+	string result = ",\"users\":[";
+	for (auto const& message :  messageMap) {
+		if (not first) result += ",";
+		result+="\"";
+		result += message.first;
+		result +="\"";
+		first = false;;
+	}
+	result += "]}";
+	messageMap[username].clear();
+	return result;
 }
 
-void handleLoggedInUsers(const Request& req, Response& res) {
-    string output = "Logged in users:\n";
-    for (User user : users) {
-        if (user.loggedIn) {
-            output += user.username + "\n";
-        }
+
+bool contains(vector<string> vec, const string & elem)
+{
+    bool result = false;
+    if( find(vec.begin(), vec.end(), elem) != vec.end() )
+    {
+        result = true;
     }
-    res.set_content(output, "text/plain");
+    return result;
+}//new contain function to check for previous emails and usernames.prof Skon pls check
+
+
+bool sixstringtest(string str) {
+    bool result = false;
+    if( str.length()<6)
+    {
+        result = true;
+    }
+    return result;
+
+}
+int main(void) {
+  Server svr;
+ userDB udb;
+
+  int nextUser=0;
+  map<string,vector<string>> messageMap;
+  map<string,string> TokenMap;
+  vector<string> emailvec;
+  vector<string> Passwordvec;
+  vector<string> Usernamevec;//new vectors???????????????????Pls Check
+  bool registered = false;
+  /* "/" just returnsAPI name */
+  svr.Get("/", [](const Request & /*req*/, Response &res) {
+    res.set_header("Access-Control-Allow-Origin","*");
+    res.set_content("Chat API", "text/plain");
+  });
+
+
+  svr.Get(R"(/chat/join/(.*)/(.*))", [&](const Request& req, Response& res) {
+    res.set_header("Access-Control-Allow-Origin","*");
+    string username = req.matches[1];
+    string pass = req.matches[2];//new
+    string result;
+    vector<string> empty;
+    cout << username << " joins" << endl;
+   if(udb.confirm(username,pass) ){//new
+	
+	string Token = generateToken();
+//	cout << "heelo" << Token;
+//	cout << "Test please work" << endl;
+	TokenMap[Token]=username;
+//gnerate token:
+ 
+//	cout<<"insideif"<<endl;
+  // if(contains(Usernamevec,username) && contains(Passwordvec,pass)){//new 
+    // Check if user with this name exists
+   // if (messageMap.count(username)) {
+    //	result = "{\"status\":\"exists\"}";
+   // } else {
+    	// Add user to messages map
+    	messageMap[username]=empty;
+    	result = "{\"status\":\"success\",\"token\":\"" + Token +"\",\"user\":\""+username+"\"}";
+    //}
+    res.set_content(result, "text/json");
+	}else{//new
+cout<<"not reg"<<endl;
+	result = "{\"status\":\"not registered\"}";
+        res.set_content(result, "text/json");
+
+    }
+
+  });
+
+   svr.Get(R"(/chat/send/(.*)/(.*))", [&](const Request& req, Response& res) {
+    res.set_header("Access-Control-Allow-Origin","*");
+	string token = req.matches[1];
+	string message = req.matches[2];
+	string result;
+	cout<<token<<endl;
+	cout<<TokenMap.count(token)<<endl;
+	cout<<TokenMap.at(token)<<endl; 
+	if(TokenMap.count(token)){
+
+string username = TokenMap.at(token);
+    if (!messageMap.count(username)) {
+    	result = "{\"status\":\"baduser\"}";
+	} else {
+		addMessage(username,message,messageMap);
+		result = "{\"status\":\"success\"}";
+	}
+}else{
+	result = "{\"status\":\"invalid token\"}";
+}
+    res.set_content(result, "text/json");
+
+  });
+  
+   svr.Get(R"(/chat/fetch/(.*))", [&](const Request& req, Response& res) {
+    string Token = req.matches[1];
+    res.set_header("Access-Control-Allow-Origin","*");
+	string resultJSON= "";
+	if(TokenMap.count(Token)){
+	string username = TokenMap.at(Token);
+	
+    string result1 = getMessagesJSON(username,messageMap);
+    string result2 = getusersJSON(username,messageMap);
+    resultJSON = result1+result2;
+}else{
+    resultJSON = "{\"status\":\"invalid token\"}";
 }
 
-int main() {
+    res.set_content(resultJSON, "text/json");
+  });
+
+
+   svr.Get(R"(/chat/register/(.*)/(.*)/(.*))", [&](const Request& req, Response& res) {
+    res.set_header("Access-Control-Allow-Origin","*");
+    string username = req.matches[1];
+    string email = req.matches[2];
+    string Password = req.matches[3];
+	udb.reg(username,email,Password);
+    string result;
+    bool usernamesuccess = false;//not sure if these will be needed
+    bool emailsuccess = false;
+    bool passwordsuccess = false;
+        // Check if user with this name exists
+    if (!contains(Usernamevec,username)) {
+    	// Add user to uservect
+    	Usernamevec.push_back(username);
+	usernamesuccess = true;
+    }
+
+    if (!contains(emailvec,email)) {
     
-    Server svr;
+    	// Add email to email vect
+    	emailvec.push_back(email);
+	emailsuccess = true;
+    }
 
-    // Serve HTML and JavaScript files
-    svr.set_mount_point("/", "./");
-    svr.set_mount_point("/", "./restChat.html");
+    if (!sixstringtest(Password)) {
+    	// Add email to password to  vect
+    	Passwordvec.push_back(Password);
+	passwordsuccess = true;
+    }
+	if(passwordsuccess && emailsuccess && usernamesuccess){
+	result = "{\"status\":\"success\"}";
+	}else{
+	result = "{\"status\":\"error\"}";
+	}    
+    res.set_content(result, "text/json");
+  });
+  
+//Require users to register and 
+  cout << "Server listening on port " << port << endl;
+  svr.listen("0.0.0.0", port);
 
-    svr.set_file_extension_and_mimetype_mapping("html", "text/html");
-    svr.set_file_extension_and_mimetype_mapping("js", "application/javascript");
-
-    // Register HTTP request handlers
-    svr.Post("/register", handleRegistration);
-    svr.Post("/login", handleLogin);
-    svr.Get("/logged-in-users", handleLoggedInUsers);
-
-    
-   svr.listen("54.198.38.17", 5005);
-
-
-    return 0;
 }
-
