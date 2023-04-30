760c925edbbcf1ed56c08f5496df9c441803707d
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2023-04-30 14:16:57 -0400

    Update restChat.js

--- restChat.js
+++ restChat.js
@@ -1,69 +1,67 @@
 $(document).ready(function() {
-  // Handle submit event for login form
-  $("#login-form").submit(function(event) {
-    // Prevent default form submission behavior
-    event.preventDefault();
-    
-    // Serialize form data
-    var formData = $(this).serialize();
-    
-    // Send AJAX request to C++ backend
-    $.ajax({
-      url: "http://54.198.38.17:5005/login",
-      type: "POST",
-      data: formData,
-      success: function(response) {
-        // Display response from server
-        $("#status").html(response);
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        // Display error message
-        $("#status").html("Error: " + textStatus + " - " + errorThrown);
-      }
-    });
-  });
-  
-  // Handle submit event for register form
-  $("#register-form").submit(function(event) {
-    // Prevent default form submission behavior
-    event.preventDefault();
-    
-    // Serialize form data
-    var formData = $(this).serialize();
-    
-    // Send AJAX request to C++ backend
-    $.ajax({
-      url: "http://54.198.38.17:5005/register",
-      type: "POST",
-      data: formData,
-      success: function(response) {
-        // Display response from server
-        $("#status").html(response);
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        // Display error message
-        $("#status").html("Error: " + textStatus + " - " + errorThrown);
-      }
-    });
-  });
+// Handle submit event for login form
+$("#login-form").submit(function(event) {
+// Prevent default form submission behavior
+event.preventDefault();
+// Serialize form data
+var formData = $(this).serialize();
+
+// Send AJAX request to C++ backend
+$.ajax({
+  url: "http://54.198.38.17:5005/login",
+  type: "POST",
+  data: formData,
+  success: function(response) {
+    // Display response from server
+    $("#status").html(response);
+    $('#status').text('successful');
+  },
+  error: function(jqXHR, textStatus, errorThrown) {
+    // Display error message
+    $("#status").html("Error: " + textStatus + " - " + errorThrown);
+  }
+});
 });
 
 
-success: function(response) {
-  // Display response from server
-$('#status').text('successful');
-}
+
+// Handle submit event for register form
+$("#register-form").submit(function(event) {
+// Prevent default form submission behavior
+event.preventDefault();
+// Serialize form data
+var formData = $(this).serialize();
+
+// Send AJAX request to C++ backend
+$.ajax({
+  url: "http://54.198.38.17:5005/register",
+  type: "POST",
+  data: formData,
+  success: function(response) {
+    // Display response from server
+    $("#status").html(response);
+    $('#status').text('successful');
+  },
+  error: function(jqXHR, textStatus, errorThrown) {
+    // Display error message
+    $("#status").html("Error: " + textStatus + " - " + errorThrown);
+  }
+});
+});
+});
 
 setInterval(function() {
-  $.ajax({
-    url: 'http://54.198.38.17:5005/logged-in-users',
-    type: 'GET',
-    success: function(users) {
-      $('#logged-in-users').empty();
-      for (var i = 0; i < users.length; i++) {
-        $('#logged-in-users').append($('<li>').text(users[i]));
-      }
-    }
-  });
+$.ajax({
+url: 'http://54.198.38.17:5005/logged-in-users',
+type: 'GET',
+success: function(users) {
+$('#logged-in-users').empty();
+for (var i = 0; i < users.length; i++) {
+$('#logged-in-users').append($('<li>').text(users[i]));
+}
+}
+});
 }, 1000);
+
+
 
