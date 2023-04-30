cb1f9256b72ccdaefca8221d086870bcee5b7eb0
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2023-04-30 15:34:23 -0400

    Update restChat.cpp

--- restChat.cpp
+++ restChat.cpp
@@ -1,93 +1,97 @@
+//
+//  namesAPI.cc - a microservice demo program
+//
+// James Skon
+// Kenyon College, 2022
+//
+
 #include <iostream>
-#include <string>
-#include <curl/curl.h>
+#include <fstream>
+#include <map>
+#include <algorithm>
+#include "httplib.h"
 
-std::string BASE_URL = "http://54.198.38.17:5005/";
+using namespace httplib;
+using namespace std;
 
-int main() {
-  // Initialize curl
-  curl_global_init(CURL_GLOBAL_DEFAULT);
+const int port = 5005;
 
-  // Login
-  CURL *curl = curl_easy_init();
-  if (curl) {
-    std::string url = BASE_URL + "login";
-    std::string body = "{\"username\": \"user\", \"password\": \"pass\"}";
+void addMessage(string username, string message, map<string,vector<string>> &messageMap) {
+	/* iterate through users adding message to each */
+	string jsonMessage = "{\"user\":\""+username+"\",\"message\":\""+message+"\"}";
+	for (auto userMessagePair : messageMap) {
+		username = userMessagePair.first;
+		messageMap[username].push_back(jsonMessage);
+	}
+}
 
-    // Set curl options
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, body.c_str());
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE_LARGE, (curl_off_t) body.size());
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, "Content-Type: application/json");
+string getMessagesJSON(string username, map<string,vector<string>> &messageMap) {
+	/* retrieve json list of messages for this user */
+	bool first = true;
+	string result = "{\"messages\":[";
+	for (string message :  messageMap[username]) {
+		if (not first) result += ",";
+		result += message;
+		first = false;
+	}
+	result += "]}";
+	messageMap[username].clear();
+	return result;
+}
 
-    // Send request
-    CURLcode res = curl_easy_perform(curl);
-    if (res != CURLE_OK) {
-      std::cerr << "Failed to send login request: " << curl_easy_strerror(res) << std::endl;
+int main(void) {
+  Server svr;
+  int nextUser=0;
+  map<string,vector<string>> messageMap;
+	
+  /* "/" just returnsAPI name */
+  svr.Get("/", [](const Request & /*req*/, Response &res) {
+    res.set_header("Access-Control-Allow-Origin","*");
+    res.set_content("Chat API", "text/plain");
+  });
+
+
+  svr.Get(R"(/chat/join/(.*))", [&](const Request& req, Response& res) {
+    res.set_header("Access-Control-Allow-Origin","*");
+    string username = req.matches[1];
+    string result;
+    vector<string> empty;
+    cout << username << " joins" << endl;
+    
+    // Check if user with this name exists
+    if (messageMap.count(username)) {
+    	result = "{\"status\":\"exists\"}";
     } else {
-      std::cout << "Login successful!" << std::endl;
+    	// Add user to messages map
+    	messageMap[username]=empty;
+    	result = "{\"status\":\"success\",\"user\":\"" + username + "\"}";
     }
+    res.set_content(result, "text/json");
+  });
 
-    // Cleanup curl
-    curl_easy_cleanup(curl);
-  } else {
-    std::cerr << "Failed to initialize curl." << std::endl;
-  }
+   svr.Get(R"(/chat/send/(.*)/(.*))", [&](const Request& req, Response& res) {
+    res.set_header("Access-Control-Allow-Origin","*");
+	string username = req.matches[1];
+	string message = req.matches[2];
+	string result; 
+	
+    if (!messageMap.count(username)) {
+    	result = "{\"status\":\"baduser\"}";
+	} else {
+		addMessage(username,message,messageMap);
+		result = "{\"status\":\"success\"}";
+	}
+    res.set_content(result, "text/json");
+  });
+  
+   svr.Get(R"(/chat/fetch/(.*))", [&](const Request& req, Response& res) {
+    string username = req.matches[1];
+    res.set_header("Access-Control-Allow-Origin","*");
+    string resultJSON = getMessagesJSON(username,messageMap);
+    res.set_content(resultJSON, "text/json");
+  });
+  
+  cout << "Server listening on port " << port << endl;
+  svr.listen("0.0.0.0", port);
 
-  // Register
-  curl = curl_easy_init();
-  if (curl) {
-    std::string url = BASE_URL + "register";
-    std::string body = "{\"username\": \"user\", \"password\": \"pass\"}";
-
-    // Set curl options
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, body.c_str());
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE_LARGE, (curl_off_t) body.size());
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, "Content-Type: application/json");
-
-    // Send request
-    CURLcode res = curl_easy_perform(curl);
-    if (res != CURLE_OK) {
-      std::cerr << "Failed to send registration request: " << curl_easy_strerror(res) << std::endl;
-    } else {
-      std::cout << "Registration successful!" << std::endl;
-    }
-
-    // Cleanup curl
-    curl_easy_cleanup(curl);
-  } else {
-    std::cerr << "Failed to initialize curl." << std::endl;
-  }
-
-  // Chat
-  curl = curl_easy_init();
-  if (curl) {
-    std::string url = BASE_URL + "chat";
-    std::string body = "{\"message\": \"Hello, world!\"}";
-
-    // Set curl options
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, body.c_str());
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE_LARGE, (curl_off_t) body.size());
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, "Content-Type: application/json");
-
-    // Send request
-    CURLcode res = curl_easy_perform(curl);
-    if (res != CURLE_OK) {
-      std::cerr << "Failed to send chat request: " << curl_easy_strerror(res) << std::endl;
-    } else {
-      std::cout << "Chat message sent." << std::endl;
-    }
-
-    // Cleanup curl
-    curl_easy_cleanup(curl);
-  } else {
-    std::cerr << "Failed to initialize curl." << std::endl;
-  }
-
-  // Cleanup curl
-  curl_global_cleanup();
-
-  return 0;
 }
