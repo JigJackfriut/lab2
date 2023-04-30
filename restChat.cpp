7ab3c398c4245589028eb41fcf98eebcab0436ca
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2023-04-30 14:38:29 -0400

    Update restChat.cpp

--- restChat.cpp
+++ restChat.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <vector>
 #include "httplib.h"
@@ -13,33 +12,10 @@ struct User {
     bool loggedIn;
 };
 
-vector<User> readUsers(string filename) {
-    vector<User> users;
-    ifstream inputFile(filename);
-    if (inputFile.is_open()) {
-        string username, password;
-        while (inputFile >> username >> password) {
-            User user = {username, password, false};
-            users.push_back(user);
-        }
-        inputFile.close();
-    }
-    return users;
-}
-
-void writeUsers(vector<User> users, string filename) {
-    ofstream outputFile(filename);
-    if (outputFile.is_open()) {
-        for (User user : users) {
-            outputFile << user.username << " " << user.password << endl;
-        }
-        outputFile.close();
-    }
-}
-
+vector<User> users;
 
 // Function to check if a username exists in the user list
-bool usernameExists(vector<User> users, string username) {
+bool usernameExists(string username) {
     for (User user : users) {
         if (user.username == username) {
             return true;
@@ -49,7 +25,7 @@ bool usernameExists(vector<User> users, string username) {
 }
 
 // Function to check if a username and password match a user in the user list
-bool authenticateUser(vector<User>& users, string username, string password) {
+bool authenticateUser(string username, string password) {
     for (User& user : users) { // Use reference to modify login status
         if (user.username == username && user.password == password) {
             user.loggedIn = true;
@@ -60,26 +36,21 @@ bool authenticateUser(vector<User>& users, string username, string password) {
 }
 
 void handleRegistration(const Request& req, Response& res) {
-    string filename = "users.txt";
-    vector<User> users = readUsers(filename);
     string username = req.get_param_value("username");
     string password = req.get_param_value("password");
-    if (usernameExists(users, username)) {
+    if (usernameExists(username)) {
         res.set_content("Username already exists.", "text/plain");
     } else {
         User newUser = {username, password, false};
         users.push_back(newUser);
-        writeUsers(users, filename);
         res.set_content("Registration successful!", "text/plain");
     }
 }
 
 void handleLogin(const Request& req, Response& res) {
-    string filename = "users.txt";
-    vector<User> users = readUsers(filename);
     string username = req.get_param_value("username");
     string password = req.get_param_value("password");
-    if (authenticateUser(users, username, password)) {
+    if (authenticateUser(username, password)) {
         res.set_content("Login successful!", "text/plain");
     } else {
         res.set_content("Invalid username or password.", "text/plain");
@@ -87,8 +58,6 @@ void handleLogin(const Request& req, Response& res) {
 }
 
 void handleLoggedInUsers(const Request& req, Response& res) {
-    string filename = "users.txt";
-    vector<User> users = readUsers(filename);
     string output = "Logged in users:\n";
     for (User user : users) {
         if (user.loggedIn) {
@@ -104,6 +73,8 @@ int main() {
 
     // Serve HTML and JavaScript files
     svr.set_mount_point("/", "./");
+    svr.set_mount_point("/", "./restChat.html");
+
     svr.set_file_extension_and_mimetype_mapping("html", "text/html");
     svr.set_file_extension_and_mimetype_mapping("js", "application/javascript");
 
@@ -112,10 +83,10 @@ int main() {
     svr.Post("/login", handleLogin);
     svr.Get("/logged-in-users", handleLoggedInUsers);
 
-    // Start server on http://0.0.0.0:8080/
-    svr.listen("0.0.0.0", 8080);
+    
+   svr.listen("54.198.38.17", 5005);
+
 
     return 0;
 }
-
 
